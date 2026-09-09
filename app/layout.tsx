ecffe8d95941746b1f672eda8b55db755ad50a12
Zubair <zubairarif062@gmail.com>
2026-09-10 01:40:21 +0500

    logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,11 @@ import './globals.css';
 
 export const metadata: Metadata = {
   title: 'NATRES | Global Commodity Trading',
+    icons: {
+    icon: '/natres-logo.png',
+    shortcut: '/natres-logo.png',
+    apple: '/natres-logo.png',
+  },
   description:
     'NATRES is an international commodity trading company headquartered in Qatar, with offices in London, Shanghai, and Johannesburg, trading Oil, Gas, LNG, LPG, and Coal.',
 };
